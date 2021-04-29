f9c5a665849f86ef02385e143240f1185a2eb262
juanCPaez <juancpaez@ucundinamarca.edu.co>
2021-04-28 20:59:16 -0500

    Creacion validaciones de contraseña en el registro

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,24 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators, FormGroupDirective, NgForm} from '@angular/forms';
-import {ErrorStateMatcher} from '@angular/material/core';
+import { FormControl, Validators, FormGroupDirective, NgForm } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 interface NumeroIdentificacion {
   value: string;
   viewValue: string;
 }
 
-
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
+
 export class RegistroComponent implements OnInit {
 
+  colorVar: string;
+  contrasena1: string;
+  contrasena2: string;
+  mensaje: string;
+  aux: boolean;
+  equalPasswordControl() {
+    this.aux = false;
+    if(this.contrasena2!='' && this.contrasena1!=''){
+    if (this.contrasena1 == this.contrasena2) {
+      this.colorVar = 'green';
+      this.mensaje = 'Las contraseñas coinciden.';
+      this.aux = true;
+    } else {
+      this.colorVar = 'red';
+      this.mensaje = 'Error: Las contraseñas no coinciden.';
+    }
+  }
+  };
+
   tiposDocumento: NumeroIdentificacion[] = [
-    {value: 'cc', viewValue: 'Cédula de ciudadanía'},
-    {value: 'ce', viewValue: 'Cédula de extranjería'},
-    {value: 'ps', viewValue: 'Pasaporte'}
+    { value: 'cc', viewValue: 'Cédula de ciudadanía' },
+    { value: 'ce', viewValue: 'Cédula de extranjería' },
+    { value: 'ps', viewValue: 'Pasaporte' }
   ];
 
   emailFormControl = new FormControl('', [
@@ -28,14 +47,16 @@ export class RegistroComponent implements OnInit {
 
   passFormControl = new FormControl('', [
     Validators.required,
-]);
-confirmFormControl = new FormControl('', [
+  ]);
+  confirmFormControl = new FormControl('', [
     Validators.required,
-    ]);
+  ]);
 
   textFormControl = new FormControl('', [
-      Validators.required,
+    Validators.required,
   ]);
+
+
   hide = true;
 
   constructor() { }
